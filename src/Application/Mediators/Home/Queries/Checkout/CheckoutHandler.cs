2c6f47084d51760d0219a34644a1b84267702669
Nikolai <35627380+destbg@users.noreply.github.com>
2020-02-18 11:14:29 +0200

    18.02.2020

--- src/Application/Mediators/Home/Queries/Checkout/CheckoutHandler.cs
+++ src/Application/Mediators/Home/Queries/Checkout/CheckoutHandler.cs
@@ -17,7 +17,6 @@ namespace Application.Home.Queries.Checkout
         private static readonly char[] HEXADECIMAL = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
         private const string MESSAGE_DIGEST = "SHA256";
         private const string ENCODING = "UTF-8";
-        private const string DATE_FORMAT = "yyyy-MM-dd.HH:mm:ss";
 
         public CheckoutHandler(IConfiguration configuration)
         {
@@ -28,11 +27,11 @@ namespace Application.Home.Queries.Checkout
         {
             const string pppUrl = "https://ppp-test.safecharge.com/ppp/purchase.do?";
 
-            var _params = ConstructParametersMap(ENCODING);
-            var _paramsForChecksum = ConstructParametersMapForChecksum(ENCODING);
-            var checksum = CalculateChecksum(_paramsForChecksum, _configuration.GetValue<string>("Key:SafeCharge"), ENCODING);
+            var _params = ConstructParametersMap();
+            var _paramsForChecksum = ConstructParametersMapForChecksum();
+            var checksum = CalculateChecksum(_paramsForChecksum, _configuration.GetValue<string>("Key:SafeCharge"));
             _params.Add("checksum", checksum);
-            var encoded_params = EncodeParameters(_params, ENCODING);
+            var encoded_params = EncodeParameters(_params);
 
             return Task.FromResult(
                 new CheckoutResponse
@@ -41,15 +40,13 @@ namespace Application.Home.Queries.Checkout
                 });
         }
 
-        private string EncodeParameters(Dictionary<string, string> _params, string encoding)
+        private string EncodeParameters(Dictionary<string, string> _params)
         {
             var postData = new StringBuilder();
             foreach (var param in _params)
             {
                 if (postData.Length != 0)
-                {
                     postData.Append("&");
-                }
 
                 postData.Append(HttpUtility.UrlEncode(param.Key, Encoding.GetEncoding(ENCODING)));
                 postData.Append("=");
@@ -59,69 +56,61 @@ namespace Application.Home.Queries.Checkout
             return postData.ToString();
         }
 
-        private Dictionary<string, string> ConstructParametersMap(string encoding)
-        {
-            var _params = new Dictionary<string, string>();
+        private Dictionary<string, string> ConstructParametersMap() =>
+            new Dictionary<string, string>
+            {
+                { "currency", "EUR" },
+                { "item_name_1", "5Test Product" },
+                { "item_number_1", "1" },
+                { "item_quantity_1", "1" },
+                { "item_amount_1", "50.00" },
+                { "numberofitems", "1" },
+                { "encoding", "utf-8" },
+                { "merchant_id", "4778151621448449994" },
+                { "merchant_site_id", "183073" },
+                { "time_stamp", "2019-07-08.09:55:50" },
+                { "version", "4.0.0" },
+                { "user_token_id", "niki4616@gmail.com" },
+                { "user_token", "auto" },
+                { "total_amount", "50.00" },
+                { "notify_url", "https://localhost:5001" },
+                { "success", "https://localhost:5001" },
+                { "back", "https://localhost:5001" }
+            };
 
-            var timestamp = DateTime.UtcNow.ToString(DATE_FORMAT);
+        private Dictionary<string, string> ConstructParametersMapForChecksum() =>
+            new Dictionary<string, string>
+            {
+                { "currency", "EUR" },
+                { "item_name_1", "5Test Product" },
+                { "item_number_1", "1" },
+                { "item_quantity_1", "1" },
+                { "item_amount_1", "50.00" },
+                { "numberofitems", "1" },
+                { "encoding", "utf-8" },
+                { "merchant_id", "4778151621448449994" },
+                { "merchant_site_id", "183073" },
+                { "time_stamp", "2019-07-08.09:55:50" },
+                { "version", "4.0.0" },
+                { "user_token_id", "niki4616@gmail.com" },
+                { "user_token", "auto" },
+                { "total_amount", "50.00" },
+                { "notify_url", "https://localhost:5001" },
+                { "success", "https://localhost:5001" },
+                { "back", "https://localhost:5001" }
+            };
 
-            _params.Add("currency", "EUR");
-            _params.Add("item_name_1", "5Test Product");
-            _params.Add("item_number_1", "1");
-            _params.Add("item_quantity_1", "1");
-            _params.Add("item_amount_1", "50.00");
-            _params.Add("numberofitems", "1");
-            _params.Add("encoding", "utf-8");
-            _params.Add("merchant_id", "4778151621448449994");
-            _params.Add("merchant_site_id", "183073");
-            _params.Add("time_stamp", "2019-07-08.09:55:50");
-            _params.Add("version", "4.0.0");
-            _params.Add("user_token_id", "niki4616@gmail.com");
-            _params.Add("user_token", "auto");
-            _params.Add("total_amount", "50.00");
-            _params.Add("notify_url", "https://sandbox.safecharge.com/lib/demo_process_request/response.php");
-
-            return _params;
-        }
-
-        private Dictionary<string, string> ConstructParametersMapForChecksum(string encoding)
-        {
-            var _params = new Dictionary<string, string>();
-
-            var timestamp = DateTime.UtcNow.ToString(DATE_FORMAT);
-
-            _params.Add("currency", "EUR");
-            _params.Add("item_name_1", "5Test Product");
-            _params.Add("item_number_1", "1");
-            _params.Add("item_quantity_1", "1");
-            _params.Add("item_amount_1", "50.00");
-            _params.Add("numberofitems", "1");
-            _params.Add("encoding", "utf-8");
-            _params.Add("merchant_id", "4778151621448449994");
-            _params.Add("merchant_site_id", "183073");
-            _params.Add("time_stamp", "2019-07-08.09:55:50");
-            _params.Add("version", "4.0.0");
-            _params.Add("user_token_id", "niki4616@gmail.com");
-            _params.Add("user_token", "auto");
-            _params.Add("total_amount", "50.00");
-            _params.Add("notify_url", "https://sandbox.safecharge.com/lib/demo_process_request/response.php");
-
-            return _params;
-        }
-
-        private string CalculateChecksum(Dictionary<string, string> _params, string merchantKey, string encoding)
+        private string CalculateChecksum(Dictionary<string, string> _params, string merchantKey)
         {
             var allVals = new StringBuilder();
             allVals.Append(merchantKey);
             foreach (var param in _params)
-            {
                 allVals.Append(param.Value);
-            }
 
-            return GetHash(allVals.ToString(), encoding);
+            return GetHash(allVals.ToString());
         }
 
-        private string GetHash(string text, string charset)
+        private string GetHash(string text)
         {
             byte[] bytes;
             using (var md = HashAlgorithm.Create(MESSAGE_DIGEST))
@@ -146,7 +135,6 @@ namespace Application.Home.Queries.Checkout
                 bytes = md.ComputeHash(encoded);
             }
 
-            // Output the bytes of the hash as a String (text/plain)
             var sb = new StringBuilder(2 * bytes.Length);
             for (var i = 0; i < bytes.Length; i++)
             {
